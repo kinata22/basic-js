7c7daacbd13c7f4008939a4f269fc2ca68c435a5
kinata <s@nata.cv.ua>
2022-10-15 00:27:29 +0200

    feat: seasons

--- src/what-season.js
+++ src/what-season.js
@@ -12,13 +12,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  //проверить, не был ли параметр undefined, null, 0, NaN, "" или просто false
+  if(!date) {
+    return 'Unable to determine the time of year!';
+  }
 
+  /*
+  Object.prototype.toString.call(date) возвращает собственное строковое представление данного типа объекта - в нашем случае "[object Date]". Поскольку date.toString переопределяет свой родительский метод, нам нужно .call или .apply метод из Object.prototype, который непосредственно...
+  Обходит пользовательский тип объекта с тем же именем конструктора (например: "Дата" )
+  Работает в разных контекстах JS (например, iframes) в отличие от instanceof.
+  */
+  if(Object.prototype.toString.call(date) != "[object Date]") {
+    throw new Error('Invalid date!');
+  }
+
+
+  // для проверки tricky moment и very tricky moment
+  try {
+    const month = date.getTime();
+  }
+  catch(err) {
+    throw new Error('Invalid date!');
+  }
+
+
+  //if (date && date.getMonth && date.getMonth.call) {
+    const month = date.getMonth() + 1;
+
+    if(month === 1 || month === 2 || month === 12) {
+      return 'winter';
+    }
+    if(month >= 3 && month <= 5) {
+      return 'spring';
+    }
+    if(month >= 6 && month <= 8) {
+      return 'summer';
+    }
+    if(month >= 9 && month <= 11) {
+      return 'autumn';
+    }
+  //}
 }
 
 module.exports = {
   getSeason
 };
-
