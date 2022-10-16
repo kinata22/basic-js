7e58c49c222f64478f25562db060d5f11a246ea9
kinata <s@nata.cv.ua>
2022-10-16 20:55:44 +0200

    feat: dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,22 +2,45 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  const letters = [];
+
+  if (members instanceof Array) {
+    // add first letters to array
+    members.forEach(element => {
+      if(typeof element === 'string' || element instanceof String) {
+        const elementWithoutSpaces = element.toString().trim();
+        letters.push(elementWithoutSpaces[0].toUpperCase());
+      }
+    });
+    //console.log(letters);
+
+    // sort of array
+    let letters2 = letters.sort();
+    //console.log(letters2.join(""));
+    //console.log(letters2);
+
+    return letters2.join("");
+  }
+  else {
+    return false;
+  }
+
 }
 
 module.exports = {
   createDreamTeam
 };
+
+//createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max'])
